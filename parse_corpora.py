f665c97525bd1f6c8f6ee9eebcdf9fb00bd68e6b
Rodger Benham <rodger.benham@gmail.com>
2020-05-10 15:17:32 +1000

    Actually include docs with no PMC or JSON parse because they were somehow judged

--- parse_corpora.py
+++ parse_corpora.py
@@ -14,23 +14,23 @@ with open('./trec-covid/doc-text.trec', 'w') as tfile:
             if row[0] in docids:
                 dups = row[1].split(";")
                 paths = [line[2:] for line in subprocess.check_output("find . -type f \( -name '" + row[5] + ".xml.json' -o -name " + dups[0] + ".json \)", shell=True).splitlines()]
+                tfile.write("<DOC>")
+                tfile.write("<DOCNO>")
+                tfile.write(row[0])
+                tfile.write("</DOCNO>")
+                tfile.write("<TITLE>")
+                tfile.write(row[3])
+                tfile.write("</TITLE>")
+                tfile.write("<DATE>")
+                tfile.write(row[9])
+                tfile.write("</DATE>")
+                tfile.write("<TEXT>")
+                tfile.write(row[8]) # include abstract in body as PMC doesnt have it
+                tfile.write("\n")
                 if len(paths) >= 1:
                     with open(paths[0]) as f:
                         data = json.load(f)
                     textdata = "\n".join(nested_lookup('text', data))
-                    tfile.write("<DOC>")
-                    tfile.write("<DOCNO>")
-                    tfile.write(row[0])
-                    tfile.write("</DOCNO>")
-                    tfile.write("<TITLE>")
-                    tfile.write(row[3])
-                    tfile.write("</TITLE>")
-                    tfile.write("<DATE>")
-                    tfile.write(row[9])
-                    tfile.write("</DATE>")
-                    tfile.write("<TEXT>")
                     tfile.write(textdata.encode('utf8'))
-                    tfile.write("</TEXT>")
-                    tfile.write("</DOC>")
-                else:
-                    print("ERROR processing record: " + row[0])
+                tfile.write("</TEXT>")
+                tfile.write("</DOC>")
